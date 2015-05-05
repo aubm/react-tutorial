807767c14402064d63b57bed3a7b8d30260d403e
kendo5731 <aur.baumann@gmail.com>
2015-05-05 22:54:21 +0200

    https://facebook.github.io/react/docs/tutorial.html#hook-up-the-data-model

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,7 +1,8 @@
 var React = require("react");
 var CommentBox = require("./ui/comment_box.jsx");
+var commentsService = require("./services/comments.js");
 
 React.render(
-    <CommentBox />,
+    <CommentBox data={commentsService.getComments()} />,
     document.getElementById("myAppContainer")
 );
---
+++ src/js/services/comments.js
@@ -0,0 +1,14 @@
+function getComments() {
+    return [
+        {
+            author: "Pete Hunt",
+            text: "This is one comment"
+        },
+        {
+            author: "Jordan Walke",
+            text: "This is *another* comment"
+        }
+    ];
+}
+
+exports.getComments = getComments;
